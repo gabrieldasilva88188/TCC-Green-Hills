d06b04ea386faa95e88d43b0b46033006a6d04fa
Gabriel da Silva <102740003+gabrieldasilva88188@users.noreply.github.com>
2024-07-09 15:29:13 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,22 +3,53 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Minha Página Principal</title>
-    <!-- Aqui você pode incluir seus estilos CSS específicos da página principal -->
+    <link rel="stylesheet" href="./CSS/style_relatorio.css">
+    <title>Relatório</title>
 </head>
 <body>
     <!-- Incluir o cabeçalho com a navbar usando PHP -->
     <?php include 'header.php'; ?>
 
     <!-- Conteúdo principal da página -->
-    <div class="content">
-        <h1>Bem-vindo à Minha Página</h1>
-        <p>Aqui você pode adicionar o conteúdo principal da sua página.</p>
+    <div class="secaobutton">
+        <div style="padding-left:10%; padding-right:10%">
+            <div class="botoes">
+                <input type="button" class="botaore" value="umidade">
+                <input type="button" class="botaore" value="temperatura">
+                <input type="button" class="botaore" value="luminosidade">
+            </div>
+        </div>  
+    </div>
+    
+    <div>
+        <table style="width:100%">      
+            <tr>
+                <th class="tabelahead">Tempo</th>
+                <th class="tabelahead">Mínima</th>
+                <th class="tabelahead">Media</th>
+                <th class="tabelahead">Máxima</th>
+            </tr>
+            <tr>
+                <td class="tabelaitem">Dia</td>
+                <td class="tabelaitem">Maria Anders</td>
+                <td class="tabelaitem">Germany</td>
+                <td class="tabelaitem">Germany</td>
+            </tr>
+            <tr>
+                <td class="tabelaitem">Semana</td>
+                <td class="tabelaitem">Francisco Chang</td>
+                <td class="tabelaitem">Mexico</td>
+                <td class="tabelaitem">Germany</td>
+            </tr>
+            <tr>
+                <td class="tabelaitem">Mês</td>
+                <td class="tabelaitem">Francisco Chang</td>
+                <td class="tabelaitem">Mexico</td>
+                <td class="tabelaitem">Germany</td>
+            </tr>
+        </table>
     </div>
 
-    <!-- Rodapé (Footer) -->
-    <footer>
-        <p>&copy; 2024 Minha Empresa. Todos os direitos reservados.</p>
-    </footer>
+
 </body>
 </html>
