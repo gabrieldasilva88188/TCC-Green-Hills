0994097fa659cabc23beeff134b85aa3519bd2df
Gabriel da Silva <102740003+gabrieldasilva88188@users.noreply.github.com>
2024-07-18 10:38:45 -0300

    Update login.php

--- login.php
+++ login.php
@@ -10,79 +10,7 @@
     <link rel="stylesheet" href="./fontawesome-free-6.5.2-web/css/all.min.css">
     <script src="./fontawesome-free-6.5.2-web/js/all.min.js"></script>
     <style>
-        .caixa{
-            margin: auto;
-            width: 500px;
-            height: 400px;
-            border-radius: 50px;
-            background-color: #82AD99;
-            position: absolute;
-            left: 50%;
-            top: 50%;
-            transform: translate(-50%,-50%);
-            box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
-        }
-        h1{
-            position: absolute;
-            left: 50%;
-            top: 10%;
-            transform: translate(-50%,-10%);
-        }
-        .botao{
-            background-color: #5C8F78;
-            width: 400px;
-            height: 50px;
-            border-radius: 50px;
-            margin: 50px;
-            margin-top: 0px;
-            margin-bottom:25px ;
-            box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
-
-        }
-        .usu{
-            position: absolute;
-            width: 50px;
-            height: 50px;
-            left: 50%;
-            top: 0%;
-            transform: translate(-50%,-50%);
-        }
-        .bolabranca{
-            width: 80px;
-            height: 80px;
-            border-radius: 50%;
-            background-color: white;
-            position: absolute;
-            left: 50%;
-            top: 0%;
-            transform: translate(-50%,-50%);
-            box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
-        }
-        .botao:hover{
-            background-color: #486256
-        }
-        p{
-            margin-left: 150px;
-        }
-        label{
-            width: 300px;
-            height: 20px;
-        }
-        .campos{
-            width: 400px;
-            height: 20px;
-            border-radius: 50px;
-            /*background-color: #5C8F78;*/
-            box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
-        }
-        *{
-        padding: 0;
-        margin: 0;
-        box-sizing: border-box;
-        font-family: "Poppins", sans-serif;
-        list-style: none;
-        text-decoration: none;
-        }
+        
     </style>
 </head>
 <body>
@@ -105,9 +33,9 @@
                     Remember-me    <a href="recuperar_senha.php" style="margin-left: 120px;">Forgot PassWord</a>
                 </label>
 
-              </div>
+            </div>
                 <input class="botao"type="button" title="btLogin" value="Login" required>
-                <p>Crie sua conta <a href="cadastro.php">Clique aqui</a></p>
+                <p class="paragrafo">Crie sua conta <a href="cadastro.php">Clique aqui</a></p>
         </div>
     </form>
     
